e7088f4b7f0c59ef317a5427ce11c066340c54ac
Karl Janke <karl.janke@groundprobe.com>
2016-05-22 23:46:49 +1000

    - Add file chooser thumb view from https://github.com/kivy-garden/garden.filechooserthumbview

--- main.py
+++ main.py
@@ -7,73 +7,19 @@ from kivy.vector import Vector
 from kivy.clock import Clock
 from kivy.config import Config
 from random import randint
-
-class PongPaddle(Widget):
-    score = NumericProperty(0)
-
-    def bounce_ball(self, ball):
-        if self.collide_widget(ball):
-            vx, vy = ball.velocity
-            offset = (ball.center_y - self.center_y) / (self.height / 2)
-            bounced = Vector(-1 * vx, vy);
-            vel = bounced * 1.1
-            ball.velocity = vel.x, vel.y + offset
-
-class PongBall(Widget):
-    # Velocity of the ball on x and y axis
-    velocity_x = NumericProperty(0)
-    velocity_y = NumericProperty(0)
-
-    # referencelist property so we can use ball.velocity as
-    # a shorthand, just like e.g. w.pos for w.x and w.y
-    velocity = ReferenceListProperty(velocity_x, velocity_y)
-
-    # `move` function will move the ball one step. This
-    # will be called in equal intervals to animate the ball
-    def move(self):
-        self.pos = Vector(*self.velocity) + self.pos
-
-class PongGame(Widget):
-    ball = ObjectProperty(None)
-    player1 = ObjectProperty(None)
-    player2 = ObjectProperty(None)
-
-    def serve_ball(self, vel=(4, 0)):
-        self.ball.center = self.center
-        self.ball.velocity = vel
-    
-    def update(self, dt):
-        self.ball.move()
-
-        # bounce off paddles
-        self.player1.bounce_ball(self.ball)
-        self.player2.bounce_ball(self.ball)
-
-        # bounce off top and bottom
-        if (self.ball.y < 0) or (self.ball.top > self.height):
-            self.ball.velocity_y *= -1
-
-        # went of to a side to score point
-        if self.ball.x < self.x:
-            self.player2.score += 1
-            self.serve_ball(vel=(4,0))
-        if self.ball.x > self.width:
-            self.player1.score += 1
-            self.serve_ball(vel=(-4,0))
-
-    def on_touch_move(self, touch):
-        if touch.x < self.width/3:
-            self.player1.center_y = touch.y
-        if touch.x > self.width - self.width/3:
-            self.player2.center_y = touch.y
+from overview import Overview
+from pong import PongGame
 
 class MainCarousel(Carousel):
-    pongLoopTick = None
-    game = None
+    pongLoopTick = None # Pong animation loop
+    game = None # Pong game
+    overview = None # Overview panel
     
     def __init__(self):
+        # Create carousel - doesn't work so well in .kv
         Carousel.__init__(self,direction='right',loop='true')
-        self.add_widget(Label(text='Hello World'))
+        self.overview = Overview()
+        self.add_widget(self.overview)
         self.add_widget(Label(text='Hello World2'))
         self.game = PongGame()
         self.game.serve_ball()
---
+++ overview.py
@@ -0,0 +1,73 @@
+import time
+from kivy.uix.widget import Widget
+from kivy.clock import Clock
+from kivy.properties import StringProperty
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.scatter import Scatter
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from filechooserthumbview import FileChooserThumbView
+from kivy.uix.filechooser import FileChooserIconView
+from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
+
+class Overview(Widget):
+    current_time = StringProperty("Initialising")
+    picture1 = None;
+    
+    def __init__(self):
+        super(Overview, self).__init__()
+        # Setup clock update loop
+        Clock.schedule_interval(self.update_time, 1.0)
+
+        # Load pictures
+        picture1 = Picture(source='C:\\Users\\K\\Pictures\\iCloud Photos\\Downloads\\IMG_0929.JPG')
+        self.add_widget(picture1, 10)
+
+        
+
+    def update_time(self, dt):
+        self.current_time = time.strftime("%I").strip('0') + time.strftime(":%M:%S %p")
+        #print("Tick: {}".format(self.current_time))
+
+    
+
+class Picture(Scatter):
+    '''Picture is the class that will show the image with a white border and a
+    shadow. They are nothing here because almost everything is inside the
+    picture.kv. Check the rule named <Picture> inside the file, and you'll see
+    how the Picture() is really constructed and used.
+
+    The source property will be the filename to show.
+    '''
+    source = StringProperty(None)
+    popup_open = False
+
+    def on_touch_down(self, touch):
+        super(Picture, self).on_touch_down(touch)
+        print("Choose photo")
+        if self.popup_open == False:
+            imageBrowse = ImageBrowse()
+            popup = Popup(content=imageBrowse,title='Select Image',
+                          size_hint=(.8, .8))
+            self.popup_open = True
+            popup.open()
+            self.popup_open = False
+            print("Popup open is false")
+
+
+class ImageBrowse(BoxLayout):
+    
+    def __init__(self):
+        super(ImageBrowse, self).__init__()
+        self.orientation = "vertical"
+        #self.size_hint = (.9, .85)
+        #box = BoxLayout(,size=self.size,pos=self.pos)
+        #self.add_widget(box)
+        #fileChooser = FileChooserThumbView(thumbsize=64,size_hint=(1,0.8),
+        #                                   path='C:\\Users\\K\\Pictures\\iCloud Photos\\Downloads')
+        fileChooser = FileChooserIconView(path='C:\\Users\\K\\Pictures\\iCloud Photos\\Downloads')
+        self.add_widget(fileChooser)
+        print("Size: {}".format(self.size))
+        print("Size: {}".format(self.pos))
+        
