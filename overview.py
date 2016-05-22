bb3d1c0e36b6a296e3034bf516fd3ee25bc05a92
Karl Janke <karl.janke@groundprobe.com>
2016-05-23 01:01:41 +1000

    Change picture based on selection

--- overview.py
+++ overview.py
@@ -41,22 +41,43 @@ class Picture(Scatter):
     The source property will be the filename to show.
     '''
     source = StringProperty(None)
+    touch_down = False
+    touch_move = False
     popup_open = False
+    imageBrowse = None
 
     def on_touch_down(self, touch):
         super(Picture, self).on_touch_down(touch)
-        print("Choose photo")
-        if self.popup_open == False:
-            imageBrowse = ImageBrowse()
-            popup = Popup(content=imageBrowse,title='Select Image',
-                          size_hint=(.8, .8))
+        self.touch_down = True
+        print("Touch down")
+
+    def on_touch_move(self, touch):
+        super(Picture, self).on_touch_move(touch)
+        self.touch_move = True
+        print("Touch move")
+
+    def on_touch_up(self, touch):
+        super(Picture, self).on_touch_up(touch)
+        print("Touch up {}".format(self.touch_move))
+        if self.touch_move == False and self.popup_open == False and self.touch_down == True:
+            print("Choose photo")
             self.popup_open = True
+            self.imageBrowse = ImageBrowse()
+            popup = Popup(content=self.imageBrowse,title='Select Image',
+                          size_hint=(.8, .8))
+            popup.bind(on_dismiss=self.dismiss_callback)
             popup.open()
-            self.popup_open = False
-            print("Popup open is false")
+        self.touch_down = False
+        self.touch_move = False
+            
+    def dismiss_callback(self, i):
+        print("Selection: {}".format(self.imageBrowse.fileChooser.selection[0]))
+        self.source = self.imageBrowse.fileChooser.selection[0]
+        self.popup_open = False
 
 
 class ImageBrowse(BoxLayout):
+    fileChooser = None
     
     def __init__(self):
         super(ImageBrowse, self).__init__()
@@ -64,10 +85,8 @@ class ImageBrowse(BoxLayout):
         #self.size_hint = (.9, .85)
         #box = BoxLayout(,size=self.size,pos=self.pos)
         #self.add_widget(box)
-        fileChooser = FileChooserThumbView(thumbsize=128,size_hint=(1,0.8),thumbdir='c:\\temp\\thumbs',
+        self.fileChooser = FileChooserThumbView(thumbsize=128,size_hint=(1,0.8),thumbdir='c:\\temp\\thumbs',
                                            path='C:\\Users\\K\\Pictures\\iCloud Photos\\Downloads')
         #fileChooser = FileChooserIconView(path='C:\\Users\\K\\Pictures\\iCloud Photos\\Downloads')
-        self.add_widget(fileChooser)
-        print("Size: {}".format(self.size))
-        print("Size: {}".format(self.pos))
+        self.add_widget(self.fileChooser)
         
