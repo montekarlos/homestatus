409876bd6b9cc25f311a374029172b157bba1e0f
Karl Janke <karl.janke@groundprobe.com>
2016-06-01 22:47:05 +1000

    Fixes to rp backlight logic

--- HomeStatus/backlight.py
+++ HomeStatus/backlight.py
@@ -2,25 +2,25 @@ from kivy.clock import Clock
 
 class RpBacklight:
     brightness_path = "/sys/devices/platform/rpi_backlight/backlight/rpi_backlight/brightness"
-    power_path = "sys/devices/platform/rpi_backlight/backlight/rpi_backlight/bl_power"
+    power_path = "/sys/devices/platform/rpi_backlight/backlight/rpi_backlight/bl_power"
     max_brightness_path = "/sys/devices/platform/rpi_backlight/backlight/rpi_backlight/max_brightness"
 
     def __init__(self):
-        with open(self.max_brightness_path) as f:
+        with open(self.max_brightness_path, mode='r') as f:
             self.max_brightness = int(f.readline());
         self.set_brightness(150)
         self.switch_on()
 
     def switch_on(self):
-        with open(self.power_path) as f:
+        with open(self.power_path, mode='a') as f:
             f.writelines("0") # 0 to swith on
 
     def switch_off(self):
-        with open(self.power_path) as f:
+        with open(self.power_path, mode='a') as f:
             f.writelines("1") # 1 to swith on
 
     def set_brightness(self, value):
-        with open(self.brightness_path) as f:
+        with open(self.brightness_path, mode='a') as f:
             f.writelines(str(value / 100.0 * self.max_brightness))
 
 class FakeBacklight:
